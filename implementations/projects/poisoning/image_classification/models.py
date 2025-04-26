cf6c2adca4f5703ad0a8f1b8dc941aeb9e613e33
slivering <47292273+slivering@users.noreply.github.com>
2025-04-26 16:45:44 +0200

    Update ConvNet16

--- implementations/projects/poisoning/image_classification/models.py
+++ implementations/projects/poisoning/image_classification/models.py
@@ -185,14 +185,15 @@ class ConvNet16(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv1 = nn.Conv2d(3, 32, 5, 2)
-        self.conv2 = nn.Conv2d(3, 64, 5, 2)
-        self.fc1 = nn.Linear(64, 512)
+        self.conv2 = nn.Conv2d(32, 64, 5, 2)
+        self.fc1 = nn.Linear(1600, 512)
         self.fc2 = nn.Linear(512, 64)
         self.fc3 = nn.Linear(64, 10)
     
     def forward(self, x):
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
+        x = x.flatten(1)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         return self.fc3(x)
